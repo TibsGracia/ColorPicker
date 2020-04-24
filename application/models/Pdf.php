c090e98d2423c4d5c076e1a01b34c73c55c61d24
TibsGracia <54094227+TibsGracia@users.noreply.github.com>
2020-04-24 07:40:21 +0700

    test

--- application/models/Pdf.php
+++ application/models/Pdf.php
@@ -6,9 +6,9 @@ class Pdf extends CI_Model
   // get mobiles list
   public function mobileList()
   {
-    $this->db->select(array('m.id', 'm.model_no', 'm.mobile_name', 'm.company', 'm.price', 'm.mobile_category'));
-    $this->db->from('mobiles as m');
-    $query = $this->db->get();
-    return $query->result_array();
+    // $this->db->select(array('m.id', 'm.model_no', 'm.mobile_name', 'm.company', 'm.price', 'm.mobile_category'));
+    // $this->db->from('mobiles as m');
+    // $query = $this->db->get();
+    // return $query->result_array();
   }
 }
